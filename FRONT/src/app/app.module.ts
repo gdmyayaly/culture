33c87a53064c914ff77d9f09a2b23cd4cd9d1746
gdmyayaly <devyayaly@gmail.com>
2020-07-04 11:08:43 +0000

    first commit

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListecollaborateurComponent } from './pages/admin/collaborateur/listecollaborateur/listecollaborateur.component';
 import { AjoutcollaborateurComponent } from './pages/admin/collaborateur/ajoutcollaborateur/ajoutcollaborateur.component';
 import { DetailcollaborateurComponent } from './pages/admin/collaborateur/detailcollaborateur/detailcollaborateur.component';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -28,6 +28,8 @@ import { DetailcollaborateurComponent } from './pages/admin/collaborateur/detail
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
