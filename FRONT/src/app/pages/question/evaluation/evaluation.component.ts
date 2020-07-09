0ab4afd060dfdcd85d22333c34fe97977a9d699e
mbackembaye0102 <elhadjimbackembaye@gmail.com>
2020-07-09 20:22:08 +0000

    evaluation

--- FRONT/src/app/pages/question/evaluation/evaluation.component.ts
+++ FRONT/src/app/pages/question/evaluation/evaluation.component.ts
@@ -6,10 +6,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./evaluation.component.scss']
 })
 export class EvaluationComponent implements OnInit {
-
+  public barrem=
+  [
+    {id:1,valeur:1},{id:2,valeur:2},{id:3,valeur:3},{id:4,valeur:4},{id:5,valeur:5}
+  ]
+  public resultat=
+  [
+    {id:0,libelle:'perseverance',valeur:null,style:'#FF4080'},
+    {id:1,libelle:'confiance',valeur:null,style:'#FF4080'},
+    {id:2,libelle:'collaboration',valeur:null,style:'#FF4080'},
+    {id:3,libelle:'autonomie',valeur:null,style:'#FF4080'},
+    {id:4,libelle:'problemsolving',valeur:null,style:'#FF4080'},
+    {id:5,libelle:'transmission',valeur:null,style:'#FF4080'},
+    {id:6,libelle:'performance',valeur:null,style:'#FF4080'},
+  ];
+  public good=false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  reponse(partie,valeur){
+    console.log(partie);
+    console.log(valeur);
+    for (let index = 1; index <= valeur; index++) {
+      let a=partie+index;
+      document.getElementById(a).setAttribute('src','assets/etoiler.png');
+      
+    }
+    for (let index = 0; valeur < 5; index++) {
+      valeur++
+      let a=partie+valeur;
+      document.getElementById(a).setAttribute('src','assets/etoileg.png');
+      
+    }
+    // let id=partie+valeur;
+    // let a= document.getElementById(id)
+    // let rien=this.resultat.find(r=>r.libelle==partie);
+    // let value=this.resultat[rien.id].valeur;
+    // if (value==null) {
+    //   this.resultat[rien.id].valeur=valeur;
+    //   a.style.color=rien.style;
+    // }
+    // else if(value!=null){
+    //   let lastid=partie+value
+    //   document.getElementById(lastid).style.color="black";
+    //   this.resultat[rien.id].valeur=valeur;
+    //   a.style.color=rien.style;
+    // }
+  }
+  validationdonner(){
+    console.log(this.resultat);
+    let a=this.resultat.find(t=>t.valeur==null);
+    if (a) {
+      alert("Veuillez remplir tous les champs")
+    }
+    else{
+      alert("bakhe")
+    }
   }
 
 }
