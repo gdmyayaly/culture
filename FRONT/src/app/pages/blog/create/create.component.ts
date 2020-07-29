d466dd78ec3e9ff53de45666017296eebdfecbe6
mbackembaye0102 <elhadjimbackembaye@gmail.com>
2020-07-29 15:40:55 +0200

    editor

--- FRONT/src/app/pages/blog/create/create.component.ts
+++ FRONT/src/app/pages/blog/create/create.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
+import * as $ from 'jquery';
 
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
+  blogForm: FormGroup;
 
-  constructor() { }
-
+  constructor( ){
+  }
   ngOnInit() {
+    this.blogForm = new FormGroup({
+      titre: new FormControl(),
+      date: new FormControl(),
+      image: new FormControl(),
+      description: new FormControl(),
+    });
+
+
+    $(document).ready(function() {
+      $('.js-example-basic-multiple').select2({ tags: true });
+    });
+
+    $(document).ready(function(){
+      $('#summernote').summernote();
+
+    });
   }
 
+  valider() {
+    var t = (document.getElementById('summernote')as HTMLTextAreaElement).value;
+    console.log(t);
+    console.log('Données du formulaire...', this.blogForm.value);
+  }
 }
+ 
--- FRONT/src/app/pages/calendar/calendar.component.ts
+++ FRONT/src/app/pages/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-calendar',
@@ -10,6 +11,7 @@ export class CalendarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
